5ebfa1dba8bc6fd6b4ccf8ccd449fab069875686
Taimoor Khan <taimoor36372@gmail.com>
2021-05-04 04:39:19 -0700

    build error

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,18 @@ import '../components/footer/style.css';
 import '../components/contacts/style.css';
 import '../pages/home/style.css';
 import './case-studies/casestudy-list/style.css'; 
+import './case-studies/easy-level/style.css'; 
+import './case-studies/filemaker/style.css'; 
+import './case-studies/filemaker/responsivness.css'; 
+import './case-studies/maikeinc/style.css'; 
+import './case-studies/megalot/style.css'; 
+import './case-studies/multi-token/style.css'; 
+import './case-studies/speakinc/style.css'; 
+import './error/style.css'
+import './mobile-app/style.css'
+import './wordpress/style.css'
+import './single-post/style.css'
+import './post-category/style.css'
 import './index.css'
 import "./about-us/style.css";
 import "./contact-us/style.css";
@@ -17,7 +29,7 @@ import './home/style.css'
 import "./sevices/style.css";
 import "./sevices/wordpress-development/style.css";
 import "./sevices/blockchain-development/style.css";
-import "./sevices/database/style.css";
+import "./sevices/database-and-backend/style.css";
 import "./sevices/design-and-prototyping/style.css";
 import "./sevices/filemaker/style.css";
 import "./sevices/ui-development/style.css";
