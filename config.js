34cfe63755a4fc252a7269a9327bb23d927979c1
Muzz <creative.joomdev@gmail.com>
2021-05-07 12:54:28 +0500

    config file changed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
-const api_ip = 'http://206.189.215.173:2000';
-// const api_ip = 'https://api.hashtag-ca.com';
+// const api_ip = 'http://206.189.215.173:2000';
+const api_ip = 'https://api.hashtag-ca.com';
 const ui_ip = 'http://165.22.214.227:3000';
 export const myConfig = { 
 
