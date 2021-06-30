52b648984e658a62c5ef26b283bef8f980f293f7
Muzammal Rahim <creative.joomdev@gmail.com>
2021-06-30 12:05:38 +0500

    add sitemap html

---
+++ pages/sitemap.html.js
@@ -0,0 +1,289 @@
+import React, { Component } from "react";
+import Header from "../components/header/index.jsx";
+import Footer from "../components/footer/index.jsx";
+require("typeface-montserrat");
+export default class sitemap extends Component {
+  render() {
+    return (
+      <div className="Sitemap-main">
+        <Header></Header>
+        <section class="content-container">
+          <div className="container-fluid p-0 m-0 ">
+            <div className="">
+              <br />
+              <br />
+              <div className="container-fluid shopifyBg p-0 m-0 ">
+                <div className="shopify-bg-right">
+                  <div className="shopify-banner-frame">
+                    <div className="container ">
+                      <div className="row justify-content-center shopify-banner-content pl-3 pr-3">
+                        <div className="col-lg-6 col-md-12">
+                          <h1 className="main-title  title-white d-block">
+                            SITEMAP
+                          </h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div class="container">
+            <section className="main-container">
+              <div className="container">
+                <section id="section1">
+                  <a href="/sevices">
+                    <h2>Services</h2>
+                  </a>
+                  <br />
+                  <div className="row">
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                          <a href="/sevices/wordpress-development">
+                            <b>wordpress Development</b>
+                          </a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                          <a href="/sevices/filemaker">
+                            <b>FileMaker Pro Development</b>
+                          </a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                          <a href="/sevices/blockchain-development">
+                            <b>Blockchain Development</b>
+                          </a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                          <a href="/sevices/ui-development">
+                            <b>Ui Development</b>
+                          </a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                          <a href="/sevices/design-and-prototyping">
+                            <b>Design and Prototyping</b>
+                          </a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                          <a href="/sevices/database-and-backend">
+                            <b>DB & Backend Development</b>
+                          </a>
+                        </li>
+                        </ul>
+                    </div>
+                  </div>
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+                <section id="section2">
+                  <a href="/shopify-experts">
+                    <h2>Shopifys</h2>
+                  </a>
+                  <br />
+
+                  <div className="row">
+                    <div class="col-md-3">
+                    <ul></ul>
+                    </div>
+                  </div>
+
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+                <section id="section3">
+                  <a href="/blogs">
+                    <h2>Blogs</h2>
+                  </a>
+                  <br />
+                  <div className="row">
+                    <div class="col-md-3">
+                      <ul>
+                      <li>
+                          <a href="/blogs/dos-and-donts-of-css-and-html">
+                            Dos and Don'ts of CSS and HTML
+                          </a>
+                        </li>
+                        <li>
+                          <a href="/blogs/third-party-security-plugins-for-wp-websites-hosted-in-wp-engine">
+                            Third-party security plugins for WP websites hosted
+                            in WP Engine
+                          </a>
+                        </li>
+                        <li>
+                          <a href="/blogs/How-to-add-xml-sitemap">
+                            How to Add an XML Sitemap to your website
+                          </a>
+                        </li>
+                        <li>
+                          <a href="/blogs/importance-of-image-optimization-for-better-page-load">
+                            Importance of Image Optimization for better page
+                            load
+                          </a>
+                        </li>
+                        <li>
+                          <a href="/how-to-create-and-schedule-cron-jobs-in-wordpress">
+                            How to create and Schedule Cron jobs in WordPress
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div class="col-md-3">
+                      <ul>
+                        <li>
+                        <a href="/blogs/how-to-upload-videos-to-your-wordpress-3-easy-ways-to-get-started">
+                            How to upload videos to your WordPress: 3 easy ways
+                            to get started
+                          </a>
+                        </li>
+                        <li>
+                        <a href="/blogs/how-to-hide-unavailable-variant-option-in-shopify-tip-for-a-shopify-developer">
+                            How to hide unavailable variant option in Shopify –
+                            Tip for a Shopify Developer
+                          </a>
+                        </li>
+                        <li>
+                        <a href="/blogs/introduction-to-less">
+                            Introduction to LESS (CSS Framework)
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+                <section id="section4">
+                  <a href="/careers">
+                    <h2>Careers</h2>
+                  </a>
+                  <br />
+
+                  <div className="row">
+                    <div class="col-md-3">
+                    <ul></ul>
+                    </div>
+                  </div>
+
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+                <section id="section5">
+                  <a href="/casestudies">
+                    <h2>Case studies</h2>
+                  </a>
+                  <br />
+                  <div className="row">
+                    <div class="col-md-3">
+                      <ul>
+                      <li>
+                          <a href="/casestudies/maikeinc">Maike Inc</a>
+                        </li>
+                        <li>
+                          <a href="/casestudies/speakinc">SpeakInc</a>
+                        </li>
+                        <li>
+                          <a href="/casestudies/megalot">Megalot</a>
+                        </li>
+                        <li>
+                        <a href="/casestudies/filemaker">
+                            Filemaker in Action
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="col-md-3">
+                      <ul>
+                      <li>
+                          <a href="/casestudies/easy-level">Easy-Level</a>
+                        </li>
+                        <li>
+                        <a href="/casestudies/multitoken">
+                            Multi Token Wallet In Gaming
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+
+                <section id="section6">
+                  <a href="/about-us">
+                    <h2>About us</h2>
+                  </a>
+                  <br />
+
+                  <div className="row">
+                    <div class="col-md-3">
+                      <ul></ul>
+                    </div>
+                  </div>
+
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+
+                <section id="section6">
+                  <a href="/contact-us">
+                    <h2>Contact us</h2>
+                  </a>
+                  <br />
+
+                  <div className="row">
+                    <div class="col-md-3">
+                    <ul></ul>
+                    </div>
+                  </div>
+
+                  <br />
+                </section>
+                <div className="divider">
+                  <br />
+                </div>
+              </div>
+            </section>
+          </div>
+        </section>
+        <Footer></Footer>
+      </div>
+    );
+  }
+}
