821ef482175571a35e8135d74e255806f4a3f0b7
Muzz <creative.joomdev@gmail.com>
2021-07-26 15:43:21 +0500

    remove extra js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
-import Head from 'next/head'
-import React ,{useEffect} from "react"
-import { withRouter,useRouter } from 'next/router'
-import Image from 'next/image'
+import React from "react"
 
-// import 'font-awesome/css/font-awesome.min.css';
-// import styles from '../styles/Home.module.css'
-import Header from '../components/header/index'
-import Footer from '../components/footer/index.jsx'
-
-import Contact from '../components/contacts/index.jsx';
-import Link from 'next/link';
-import Carousel from '../components/carousel/index';
-import axios from 'axios'
-// import Home from '../pages/home/index'
-import Slide from 'react-reveal/Reveal';
 import Homes from './home/index'
 
 import Axios from "axios";
 import https from "https";
-
-import * as config from '../config'
 
 export async function getServerSideProps() {
 
@@ -49,7 +33,6 @@ export async function getServerSideProps() {
 export default function Home(props) {
   let response = props
   const [data, setData] = React.useState(response.data.data)
-  const router = useRouter();
   
     
   return (
