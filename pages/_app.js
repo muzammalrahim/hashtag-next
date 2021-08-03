b1715970420e57ad697dad6a726448f4d4a8854e
muzammalrahim <creative.joomdev@gmail.com>
2021-08-03 11:44:21 +0500

    adding some data for speed optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,25 @@
-// import "slick-carousel/slick/slick-theme.css";
 import './index.css';
-
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import '../node_modules/bootstrap/dist/css/bootstrap.css'
-
-// import './careers/style.css'
-// import './home/style.css';
-
-// require('typeface-montserrat')
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case "FCP":
+      // handle FCP results
+      break;
+    case "LCP":
+      // handle LCP results
+      break;
+    case "CLS":
+      // handle CLS results
+      break;
+    case "FID":
+      // handle FID results
+      break;
+    case "TTFB":
+      // handle TTFB results
+      break;
+    default:
+      break;
+  }
+}
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
