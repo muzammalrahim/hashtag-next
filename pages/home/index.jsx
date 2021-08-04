fd3505a5aaa37032fa9abcf0fe442180d0a50b1d
Muzammal Rahim <creative.joomdev@gmail.com>
2021-08-04 13:14:12 +0500

    changes in google map

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -436,40 +436,40 @@ export default class Home extends Component {
 
             <div className="desktop-only-expertise d-none d-sm-block">
               {" "}
-              <div className="row justify-content-center mt-2 homePageImgs">
+              <div className="row justify-content-center mt-2 ">
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/html5.svg"
                     alt="html5"
-                    className="img-fluid mx-auto my-auto homePageImgs "
+                    className="img-fluid mx-auto my-auto  "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/mysql.svg"
                     alt="mysql"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/amazon.svg"
                     alt="amazon"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/shopify.svg"
                     alt="shopify"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/php.svg"
                     alt="php"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
               </div>
@@ -479,35 +479,35 @@ export default class Home extends Component {
                   <img
                     src="images/home/bootstrap.svg"
                     alt="bootstrap"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/node-js.svg"
                     alt="node_js"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/angular-js.svg"
                     alt="angular_js"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/ethereum.svg"
                     alt="ethereum"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/react-js.svg"
                     alt="react_js"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
               </div>
@@ -517,35 +517,35 @@ export default class Home extends Component {
                   <img
                     src="images/home/wordpress.svg"
                     alt="wordpress"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/jquery.svg"
                     alt="jquery"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/codeigniter.svg"
                     alt="codeigniter"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/django.svg"
                     alt="django"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/python.svg"
                     alt="python"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
               </div>
@@ -555,35 +555,35 @@ export default class Home extends Component {
                   <img
                     src="images/home/mongodb.svg"
                     alt="mongodb"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5 ">
                   <img
                     src="images/home/truffle.svg"
                     alt="truffle"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/solidity.svg"
                     alt="solidity"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/docker.svg"
                     alt="docker"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
                 <div className="col-lg-2 col-md-2 col-sm-12 text-center my-auto d-flex pt-5">
                   <img
                     src="images/home/ionic.svg"
                     alt="ionic"
-                    className="img-fluid mx-auto my-auto homePageImgs"
+                    className="img-fluid mx-auto my-auto "
                   />
                 </div>
               </div>
