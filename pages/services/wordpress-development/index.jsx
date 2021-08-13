4c83e5c1f956ad79314f9995c193163a381340c2
Muzammal Rahim <creative.joomdev@gmail.com>
2021-08-13 17:29:51 +0500

    optimize wordpress page

--- pages/services/wordpress-development/index.jsx
+++ pages/services/wordpress-development/index.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
-import Header from "../../../components/header/index.jsx";
-import Footer from "../../../components/footer/index.jsx";
-import Contact from "../../../components/contacts/index.jsx";
 import Wpslider from "../../../components/wordpress-slider/index.jsx";
-import Wpcarousel from "../../../components/wp-carousel/index.jsx";
-import Wpsliderv2 from "../../../components/wordpress-sliderv2/index.jsx";
-import Testimonial from "../../../components/testimonial/index.jsx";
+
 import DocumentMeta from "react-document-meta";
-import Slide from "react-reveal/Reveal";
 import Axios from "axios";
 import https from "https";
+
+import dynamic from "next/dynamic";
+require("typeface-montserrat");
+
+
+import "lazysizes";
+// import a plugin
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+
+
+
+
+const Slide = dynamic(() => import("react-reveal/Reveal"));
+const Header = dynamic(() => import("../../../components/header/index.jsx"));
+const Footer = dynamic(() => import("../../../components/footer/index.jsx"));
+const Contact = dynamic(() => import("../../../components/contacts/index.jsx"));
+const Wpcarousel = dynamic(() => import("../../../components/wp-carousel/index.jsx"));
+const Wpsliderv2 = dynamic(() => import("../../../components/wordpress-sliderv2/index.jsx"));
+const Testimonial = dynamic(() => import("../../../components/testimonial/index.jsx"));
+
+
+
 
 export async function getServerSideProps() {
   let data = [];
@@ -62,7 +78,7 @@ export default class Shopify extends Component {
       {
         id: 3,
         src: "../images/wordpress/stories-upon.png",
-        alt: "wp theme development",
+        alt: "wp theme development"
       },
       {
         id: 4,
@@ -136,8 +152,8 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img
-                      src="../images/wordpress/web-programming.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/web-programming.svg"
                       alt="wp_dev"
                     />
                     <p className="text-level-5 title-black">
@@ -150,7 +166,7 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img src="../images/wordpress/group.svg" alt="wp_theme" />
+                    <img   class="lazyload" data-src="../images/wordpress/group.svg" alt="wp_theme" />
                     <p className="text-level-5 title-black">
                       WordPress Theme Customization
                     </p>
@@ -161,8 +177,8 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img
-                      src="../images/wordpress/puzzke.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/puzzke.svg"
                       alt="plugin_dev"
                     />
                     <p className="text-level-5 title-black">
@@ -175,7 +191,7 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img src="../images/wordpress/psd-wp.svg" alt="psd_to_wp" />
+                    <img   class="lazyload" data-src="../images/wordpress/psd-wp.svg" alt="psd_to_wp" />
                     <p className="text-level-5 title-black">
                       PSD to WordPress Conversion
                     </p>
@@ -189,8 +205,8 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img
-                      src="../images/wordpress/html-wp.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/html-wp.svg"
                       alt="html_to_wp"
                     />
                     <p className="text-level-5 title-black">
@@ -203,7 +219,7 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img src="../images/wordpress/development.svg" alt="hire" />
+                    <img   class="lazyload" data-src="../images/wordpress/development.svg" alt="hire" />
                     <p className="text-level-5 title-black">
                       Hire WordPress Developer
                     </p>
@@ -214,8 +230,8 @@ export default class Shopify extends Component {
               <Slide bottom>
                 <div className="col-lg-3 col-sm-6 mob-padding">
                   <div className="what-we-do-border-style what-we-do-responsive">
-                    <img
-                      src="../images/wordpress/education.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/education.svg"
                       alt="training"
                     />
                     <p className="text-level-5 title-black">
@@ -259,8 +275,8 @@ export default class Shopify extends Component {
                       </p>
                     </p>
                     <div className="main-title">
-                      <img
-                        src="../images/wordpress/wp-quoute.svg"
+                      <img   class="lazyload"
+                        data-src="../images/wordpress/wp-quoute.svg"
                         alt="quote"
                       />
                     </div>
@@ -301,9 +317,9 @@ export default class Shopify extends Component {
                   </div>
 
                   <div className="col-12 col-lg-5 col-sm-6 col-md-12 expert-img-section">
-                    <img
+                    <img   class="lazyload"
                       className="experts-img"
-                      src="../images/wordpress/tony-hashtag-sys.png"
+                      data-src="../images/wordpress/tony-hashtag-sys.png"
                       alt="tony-hashtag"
                     />
                     <p className="shopify-title title-margin text-center text-level-4">
@@ -345,59 +361,59 @@ export default class Shopify extends Component {
               <div class="desktop-only-expertise d-none d-sm-block">
                 <div className="row justify-content-center about-row pl-3 pr-3 pt-4">
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/acf-plugin.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/acf-plugin.svg"
                       alt="acf-plugin"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/all-in-one-seo.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/all-in-one-seo.svg"
                       alt="all-in-one-seo"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/autoptimize.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/autoptimize.svg"
                       alt="autoptimize"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img src="../images/wordpress/bbpres.svg" alt="bbpres" />{" "}
+                    <img   class="lazyload" data-src="../images/wordpress/bbpres.svg" alt="bbpres" />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/buddy-press.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/buddy-press.svg"
                       alt="buddy-press"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/classic-editor.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/classic-editor.svg"
                       alt="classic-editor"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/cloudfare.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/cloudfare.svg"
                       alt="cloudfare"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/disable-comment.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/disable-comment.svg"
                       alt="disable-comment"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/event-manager.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/event-manager.svg"
                       alt="event-manager"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/acf-plugin.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/acf-plugin.svg"
                       alt="acf-plugin"
                     />{" "}
                   </div>
@@ -405,105 +421,105 @@ export default class Shopify extends Component {
 
                 <div className="row justify-content-center about-row pl-3 pr-3 pt-4">
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/featured-video.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/featured-video.svg"
                       alt="featured-video"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/filemanager.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/filemanager.svg"
                       alt="filemanager"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/google-map.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/google-map.svg"
                       alt="google-map"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/gravity-form.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/gravity-form.svg"
                       alt="gravity-form"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img src="../images/wordpress/jetpack.svg" alt="jetpack" />{" "}
+                    <img   class="lazyload" data-src="../images/wordpress/jetpack.svg" alt="jetpack" />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/layer-slider.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/layer-slider.svg"
                       alt="layer-slider"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/learnpress.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/learnpress.svg"
                       alt="learnpress"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/mailichimp.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/mailichimp.svg"
                       alt="mailchimp"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/openmenu.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/openmenu.svg"
                       alt="openmenu"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/paid-member-pro.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/paid-member-pro.svg"
                       alt="paid-member-pro"
                     />{" "}
                   </div>
                 </div>
                 <div className="row justify-content-center about-row pl-3 pr-3 pt-4">
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/pdf-plugin.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/pdf-plugin.svg"
                       alt="pdf-plugin"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/slider-revolution.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/slider-revolution.svg"
                       alt="slider-revolution"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img src="../images/wordpress/smush.svg" alt="smush" />{" "}
+                    <img   class="lazyload" data-src="../images/wordpress/smush.svg" alt="smush" />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img src="../images/wordpress/ssl.svg" alt="ssl" />{" "}
+                    <img   class="lazyload" data-src="../images/wordpress/ssl.svg" alt="ssl" />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/woocommerce.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/woocommerce.svg"
                       alt="woocommerce"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/woocommerce-service.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/woocommerce-service.svg"
                       alt="woocommerce-service"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img
-                      src="../images/wordpress/word-defence.svg"
+                    <img   class="lazyload"
+                      data-src="../images/wordpress/word-defence.svg"
                       alt="word-defence"
                     />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img src="../images/wordpress/wp-page.svg" alt="wp-page" />{" "}
+                    <img   class="lazyload" data-src="../images/wordpress/wp-page.svg" alt="wp-page" />{" "}
                   </div>
                   <div class="col-lg-1 col-md-1">
-                    <img src="../images/wordpress/yoast.svg" alt="yoast" />{" "}
+                    <img   class="lazyload" data-src="../images/wordpress/yoast.svg" alt="yoast" />{" "}
                   </div>
                 </div>
               </div>
