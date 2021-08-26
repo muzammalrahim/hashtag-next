3de206f1b4924723de694d4d27c3223e5db3a996
Muzammal Rahim <creative.joomdev@gmail.com>
2021-08-26 18:07:52 +0500

    wordpress

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -90,7 +90,7 @@ export default class Home extends Component {
         </div>
 
         <div className="d-block d-xl-none">
-      <p className="tempara"> your <br/> page <br/> are <br/> loading  <br/>........ </p>
+      {/* <p className="tempara"> your <br/> page <br/> are <br/> loading  <br/>........ </p> */}
           <div className="container-fluid mobile-banner">
             {/* <img src="/images/home/home-mobile-banner.webp" /> */}
             <div className="row justify-content-center">
